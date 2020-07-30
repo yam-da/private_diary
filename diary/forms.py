8677d3c05c408ec16dd33778fa037776a7947fd2
s.y <flwflite2@gmail.com>
2020-07-30 22:46:16 +0900

    フォームのフィールドを追加 forms.py追加

---
+++ diary/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class InquiryForm(forms.Form):
+    name = forms.CharField(lavel='お名前', max_length=30)
+    email = forms.EmailField(label='メールアドレス')
+    title = forms.CharField(label='タイトル', max_length=30)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(**args, **kwargs)
+
+        self.fields['name'].widget.attrs['class'] = 'form-control col-9'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力して下さい'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力して下さい'
+
+        self.fields['title'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力して下さい'
+
+        self.fields['message'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力して下さい'
